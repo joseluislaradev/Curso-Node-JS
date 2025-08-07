b9ee5cc250f1eb3c73f427314411625ed62dae1c
perezlarajose14@gmail.com <perezlarajose14@gmail.com>
2025-08-06 22:58:09 -0600

    Middleware en express clase 2

--- clase2/3.express.js
+++ clase2/3.express.js
@@ -2,6 +2,26 @@
 const express = require("express");
 const app = express();
 
+// //El middleware es una funcion que sirve para interceptar las peticiones y respuestas, permitiendo modificar o procesar los datos antes de que lleguen al manejador final.
+// app.use((req, res, next) => { //Sepone .use perouede poner un metodo especifico para que se ejecute solo para ese verbo http, o poner como primer argumento un path para que se ejecute solo para ese path
+//   if (req.method != "POST") return next(); //next() es una funcion que se debe llamar para que la peticion continue al siguiente middleware o manejador de ruta, si no se llama, la peticion se queda y no recibe respuesta ni sigue.
+//   if (req.headers["content-type"] != "application/json") return next(); //Si el content-type no es application/json, se salta este middleware
+
+//   //Extrayendo logica de un post en un middleware para no repetir codigo
+//   let body = "";
+//   req.on("data", (chunk) => {
+//     body += chunk.toString();
+//   });
+//   req.on("end", () => {
+//     const data = JSON.parse(body);
+//     //mutar la request y meter la info en el request.body
+//     req.body = data;
+//     next();
+//   });
+// });
+
+app.use(express.json()); //Middleware por defecto de express para parsear el cuerpo de las peticiones a JSON, si no se pone, no se puede acceder al req.body
+
 app.disable("x-powered-by"); //Deshabilita la cabecera x-powered-by que indica que el servidor está usando Express, por razones de seguridad
 
 const dittoJson = require("./ditto.json");
@@ -16,14 +36,7 @@ app.get("/pokemon/ditto", (req, res) => {
 });
 
 app.post("/pokemon", (req, res) => {
-  let body = "";
-  req.on("data", (chunk) => {
-    body += chunk.toString();
-  });
-  req.on("end", () => {
-    const pokemon = JSON.parse(body);
-    res.status(201).json(pokemon);
-  });
+  res.status(201).json(req.body); //201 es el estatus por defecto para crear un recurso
 });
 
 //Forma global de tratar rutas no definidas, siempre va buscando ade arriba a abajo por eso debe ser la ultima
